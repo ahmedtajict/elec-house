b6cb80acd302f0c9ab65aaf45669effe51148a1c
ahmedtajict <ahmedtajict@gmail.com>
2022-08-25 19:14:29 +0600

    Electronic warehoue website setup

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#31f5e1",
+          secondary: "#72fcef",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
